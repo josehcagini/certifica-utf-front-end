d2bc69cfa7002dfb39c67e852e5ca83da0b5bdc3
Cagnini <jose.henriquec523@gmail.com>
2023-10-24 17:41:23 -0300

    fix nextauth para Route handler Next@13.2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,9 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 
-import { authOptions } from '../../pages/api/auth/[...nextauth]'
+import { authOptions } from './api/auth/[...nextauth]/route'
 import { getServerSession } from 'next-auth'
-import SessionProvider from '../../pages/sessionProvider'
+import SessionProvider from '../components/sessionProvider/session'
 import Login from './login/page'
 import Navbar from '@/components/navbar/navbar'
 
