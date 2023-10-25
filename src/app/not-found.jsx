d5e9499d4984ee58063c6a350f5423e1c2bc94a5
jose.henrique <jose.henriquec523@gmail.com>
2023-10-24 21:14:41 -0300

    add tratamento de erro em chamadas http, not-found

---
+++ src/app/not-found.jsx
@@ -0,0 +1,9 @@
+import styles from './page.module.css'
+
+export default function NotFound(){
+    return (
+        <div className={styles.flexCenter}>
+            <h1>Pagina não encontrada</h1>
+        </div>
+    )
+}
