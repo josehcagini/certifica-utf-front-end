94c9ea6b4f59a67e2fdc8bd8918d888ce1d4022a
Alvaro Eduardo Menegon Rosario <104403541+alvaromenegon@users.noreply.github.com>
2023-11-20 15:01:47 -0300

    criada a pagina de emitir certificados

--- src/app/certificado/emitir/page.js
+++ src/app/certificado/emitir/page.js
@@ -1,7 +1,98 @@
-export default function EmitirCertificado(){
+'use client'
+import ItemList from "@/components/itemList"
+import { useEffect, useState } from "react";
+import gridStyle from '../../evento/gerenciar/styles.module.css'
+import Button, { ButtonType } from "@/components/button";
+import styles from './styles.module.css';
+import { fetchData } from "@/app/api/utils/apiUtils";
+
+export default function EmitirCertificado() {
+    const [certificados, setCertificados] = useState([])
+    const [checked, setChecked] = useState([]);
+
+    const handleCheck = (e) => {
+        let id = e.target.id.split('-')[1];
+        let pseudoCheckbox = document.getElementById('pseudoCheckbox-' + id);
+        if (e.target.checked) {
+            pseudoCheckbox.classList.add(styles.checked);
+            setChecked([...checked, id]);
+        } else {
+            pseudoCheckbox.classList.remove(styles.checked);
+            setChecked(checked.filter((item) => item !== id));
+        }
+    }
+    
+    const emitir = async () => {
+        const response = await fetchData(`${process.env.API_BASE_URL}/certificado/emitir`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ids: checked}),
+        })
+        const data = await response.json()
+        if (response.status === 200) {
+            alert('Certificado emitido com sucesso');
+            location.reload();
+
+        } else {
+            alert('Erro ao emitir certificado')
+        }
+    }
+
+    const getCertificados = async () => {
+        const response = await fetchData(`${process.env.API_BASE_URL}/certificados/emitir`)
+        const data = await response.json()
+        if (response.status === 200) {
+            setCertificados(data);
+        } else {
+            alert('Erro ao buscar certificados')
+        }
+    };
+
+    useEffect(() => {
+        getCertificados()
+    },[]);
+
     return (
-        <div>
-            Emitir certificado
+        <div className="main">
+            <h1>Certificados Disponíveis</h1>
+            <div className={gridStyle.grid}>
+                { certificados.length !==0 ?
+                    certificados.map((certificado) => {
+                        return (
+                            <ItemList
+                                key={certificado.id}
+                                title={certificado.dsTitulo}
+                                subtitle={certificado.dtConclusao}
+                            >
+                                <label
+                                    className={styles.labelBtnCheck}
+                                    id={'pseudoCheckbox-' + certificado.id}
+                                    htmlFor={'certificado-' + certificado.id}
+                                ></label>
+                                <input
+                                    type="checkbox"
+                                    onChange={handleCheck}
+                                    className={styles.btnCheck}
+                                    id={'certificado-' + certificado.id}
+                                />
+                            </ItemList>
+                        )
+                    }) :
+                    <h2>Nenhum certificado disponível</h2>
+                }
+            </div>
+            {checked.length !== 0 &&
+            <Button
+                onClick={emitir}
+                isEnabled={true}
+                styletype={ButtonType.SECONDARY}
+                style={{ marginTop: '2.5rem' }}
+            >
+                Emitir Certificado(s)
+            </Button>}
+
         </div>
     )
 }
--- src/components/itemList/index.js
+++ src/components/itemList/index.js
@@ -1,26 +1,32 @@
-import Button, { ButtonType } from '../button';
+import Button from '../button';
 import styles from './styles.module.css';
 export default function ItemList(props) {
     const { title, subtitle, onClick, buttonTitle, buttonStyletype } = props;
+    const { children } = props;
     return (
         <div className={styles.itemList}>
-            <div className={styles.itemListContent}>
-                <p className={styles.title}>
-                    {title}
-                </p>
-                <p className={styles.subtitle}>
-                    {subtitle}
-                </p>
+            <div className={styles.before}>
+                {children}
+                <div className={styles.itemListContent}>
+                    <p className={styles.title}>
+                        {title}
+                    </p>
+                    <p className={styles.subtitle}>
+                        {subtitle}
+                    </p>
+                </div>
             </div>
-            <div>
-                <Button
-                    onClick={onClick}
-                    isEnabled={true}
-                    styletype={buttonStyletype}
-                >
-                    {buttonTitle}
-                </Button>
-            </div>
+            {buttonTitle &&
+                <div>
+                    <Button
+                        onClick={onClick}
+                        isEnabled={true}
+                        styletype={buttonStyletype}
+                    >
+                        {buttonTitle}
+                    </Button>
+                </div>
+            }
         </div>
     )
 }
