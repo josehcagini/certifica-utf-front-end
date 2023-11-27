43901b78268f05da5d2c5341e5e27738a2f6d87c
Alvaro Eduardo Menegon Rosario <104403541+alvaromenegon@users.noreply.github.com>
2023-11-27 15:51:16 -0300

    add user id ao session e useEffect na tela inicial

--- src/app/page.js
+++ src/app/page.js
@@ -1,52 +1,56 @@
 'use client';
 import styles from './page.module.css'
 import { useSession } from 'next-auth/react';
-import { BiPlus, BiLocationPlus } from 'react-icons/bi' ;
-import { PiNotebookDuotone } from 'react-icons/pi' ;
-import { AiOutlineImport } from 'react-icons/ai' ;
-import { LuCalendarSearch, LuCalendarPlus, LuCalendar } from 'react-icons/lu' ;
+import { BiPlus, BiLocationPlus } from 'react-icons/bi';
+import { PiNotebookDuotone } from 'react-icons/pi';
+import { AiOutlineImport } from 'react-icons/ai';
+import { LuCalendarSearch, LuCalendarPlus, LuCalendar } from 'react-icons/lu';
 import { FaWpforms } from 'react-icons/fa'
 import Card from '@/components/card';
 import { isAdmin } from '@/services/user/userService';
+import { useEffect, useState } from 'react';
 
 
 export default function Home() {
-
   const session = useSession();
+  const [isAdminUser, setIsAdminUser] = useState(false);
 
-  // TODO chamar backend para cadastrar o usuario, analisar melhor como vai funcionar o esquema de autenticação
-  console.log(session.data);
+  useEffect(() => {
+    const adminUser = isAdmin(session?.data?.user?.roles ?? []);
+    setIsAdminUser(adminUser);
+  }, [session])
+
   return (
     <div className={styles.content}>
-      { !isAdmin( session?.data?.user?.roles ?? [] ) ? 
+      {!isAdminUser ?
         <>
           <div className={styles.containerTitle}>
             <h1>Certificados</h1>
           </div>
           <div className={styles.container}>
-            <Card title="Emitir Certificado" icon={<BiPlus size={35}/>} link="/certificado/emitir"/>
-            <Card title="Visualizar Certificado" icon={<PiNotebookDuotone size={35}/>} link="/certificado/emitir"/>
-            <Card title="Importar Certificado" icon={<AiOutlineImport size={35}/>} link="/certificados"/>
+            <Card title="Emitir Certificado" icon={<BiPlus size={35} />} link="/certificado/emitir" />
+            <Card title="Visualizar Certificado" icon={<PiNotebookDuotone size={35} />} link="/certificado/emitir" />
+            <Card title="Importar Certificado" icon={<AiOutlineImport size={35} />} link="/certificados" />
           </div>
         </> : <></>
       }
       <div className={styles.containerTitle}>
         <h1>Eventos</h1>
       </div>
-      { !isAdmin( session?.data?.user?.roles ?? [] ) ? 
+      {!isAdminUser ?
         <div className={styles.container}>
-          <Card title="Eventos Disponíveis" icon={<LuCalendarSearch size={35}/>} link="/evento/listar"/>
-          <Card title="Inscrições" icon={<FaWpforms size={35}/>} link="/evento/inscritos"/>
-          <Card title="Fazer Check-in" icon={<BiLocationPlus size={35}/>} link="/checkin"/>
-        </div> 
-        : 
-        <div className={styles.container}>
-          <Card title="Ver Eventos" icon={<LuCalendarSearch size={35}/>} link="/evento/criar"/>
-          <Card title="Gerenciar Eventos" icon={<LuCalendar size={35}/>} link="/evento/gerenciar"/>
-          <Card title="Criar Eventos" icon={<LuCalendarPlus size={35}/>} link="/evento/criar"/>
+          <Card title="Eventos Disponíveis" icon={<LuCalendarSearch size={35} />} link="/evento/listar" />
+          <Card title="Inscrições" icon={<FaWpforms size={35} />} link="/evento/inscritos" />
+          <Card title="Fazer Check-in" icon={<BiLocationPlus size={35} />} link="/checkin" />
         </div>
-      } 
+        :
+        <div className={styles.container}>
+          <Card title="Ver Eventos" icon={<LuCalendarSearch size={35} />} link="/evento/criar" />
+          <Card title="Gerenciar Eventos" icon={<LuCalendar size={35} />} link="/evento/gerenciar" />
+          <Card title="Criar Eventos" icon={<LuCalendarPlus size={35} />} link="/evento/criar" />
+        </div>
+      }
     </div>
-    
+
   )
 }
