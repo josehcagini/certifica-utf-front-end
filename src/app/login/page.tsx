e025c9088ca1d10b394049de732e18fca5a9dbd3
Felipe Shimada <felipe.shimada@kepha.com.br>
2024-11-10 14:21:17 -0300

    feat: CERUTF-107 adcionar responsividade

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,21 +4,22 @@ import LoginForm from './_components/LoginForm'
 
 export default function Login() {
   return (
-    <div className="flex h-screen bg-gray-100">
-      <div className="flex w-1/2 flex-col justify-between bg-black p-12 text-white">
+    <div className="flex h-screen flex-col bg-gray-100 lg:flex-row">
+      <div className="hidden w-full flex-col justify-between bg-black p-8 text-white lg:flex lg:w-1/2 lg:p-12">
         <div>
           <GaugeIcon className="h-8 w-8 text-white" />
           <h1 className="mt-4 text-3xl font-semibold">Acme Inc</h1>
         </div>
-        <div>
-          <blockquote>
+        <div className="mt-6 lg:mt-0">
+          <blockquote className="text-base leading-relaxed">
             "This library has saved me countless hours of work and helped me
             deliver stunning designs to my clients faster than ever before."
           </blockquote>
-          <p className="mt-4">Sofia Davis</p>
+          <p className="mt-4 text-sm">Sofia Davis</p>
         </div>
       </div>
-      <div className="flex w-1/2 items-center justify-center p-12">
+
+      <div className="flex w-full items-center justify-center p-8 lg:w-1/2 lg:p-12">
         <div className="w-full max-w-md">
           <div className="rounded-lg bg-white px-6 py-8 shadow sm:px-10">
             <div className="mb-8">
